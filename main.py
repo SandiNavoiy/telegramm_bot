1901302267df2f65c93ed631c33fc40ee37b6bf1
Sandi_Navoiy <santes.au@gmail.com>
2023-05-21 15:59:59 +0300

    2

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import telebot
 import config
 
 
-bot = telebot.TeleBot('6033109078:AAEm9HFaZT8UrMCQcdcE5BI5Kt56NNCkwRO')
+bot = telebot.TeleBot('6033109078:AAEm9HFaZT8UrMCQcdcE5BI5Kt56NNCkwR0')
 
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я на связи. Напиши мне что-нибудь )')
+# Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
-def repeat_all_messages(message):
-    bot.send_message(message.chat.id, message.text)
-
-if __name__ == '__main__':
-    bot.infinity_polling(True)
+def handle_text(message):
+    bot.send_message(message.chat.id, 'Вы написали: ' + message.text)
+# Запускаем бота
+bot.polling(none_stop=True, interval=0)
