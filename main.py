dd26bf445cf362f2413290cc151cf11b26f3618a
Sandi_Navoiy <santes.au@gmail.com>
2023-05-21 15:49:45 +0300

    2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
- gfmngmh
+import telebot
+import config
+
+
+bot = telebot.TeleBot('6033109078:AAEm9HFaZT8UrMCQcdcE5BI5Kt56NNCkwRO')
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.infinity_polling(True)
