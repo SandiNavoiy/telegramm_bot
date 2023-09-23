d9d4db3d0e7ed1be98cf9ca7767646733764942e
Sandi_Navoiy <santes.au@gmail.com>
2023-09-23 20:05:51 +0300

    маленький фикс

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ bot = telebot.TeleBot('6033109078:AAEm9HFaZT8UhMCQcdcE5BI5Kt56NNCkwR0')
 
 @bot.message_handler(commands=["start"])
 def start(m, res=False):
-    bot.send_message(m.chat.id, 'Я на связи. Веди название валюты - Usd или Eur )')
+    bot.send_message(m.chat.id, 'Я на связи. Веди название валюты - Usd, Eur, Cad, Gbp )')
 
 
 # Получение сообщений от юзера
 @bot.message_handler(content_types=["text"])
 def handle_text(message):
-    if message.text not in ["Usd", "Eur"]:
-        bot.send_message(message.chat.id, 'Некорректный ввод: напишите "Usd", "Eur"')
+    if message.text not in ["Usd", "Eur", "Cad", "Gbp"]:
+        bot.send_message(message.chat.id, 'Некорректный ввод: напишите "Usd", "Eur","Cad", "Gbp"')
 
 
     elif message.text == "Usd":
@@ -37,6 +37,21 @@ def handle_text(message):
         data = response.json()
         rate = data["rates"]["RUB"]
         bot.send_message(message.chat.id, f'Курс EUR к рублю: {rate}')
+        time.sleep(30)
+    elif message.text == "Cad":
+        url = f"https://api.apilayer.com/exchangerates_data/latest?base={message.text}"
+        response = requests.get(url, headers={'apikey': API_KEY})
+        data = response.json()
+        rate = data["rates"]["RUB"]
+        bot.send_message(message.chat.id, f'Курс CAD к рублю: {rate}')
+        time.sleep(30)
+
+    elif message.text == "Gbp":
+        url = f"https://api.apilayer.com/exchangerates_data/latest?base={message.text}"
+        response = requests.get(url, headers={'apikey': API_KEY})
+        data = response.json()
+        rate = data["rates"]["RUB"]
+        bot.send_message(message.chat.id, f'Курс GBP к рублю: {rate}')
         time.sleep(30)
 
 
